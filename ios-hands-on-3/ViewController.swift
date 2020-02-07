4abb78251da4a5811264bc23d8f2a513cf62afa4
Junya Kawai <junya.kawai@nextbeat.net>
2020-02-07 14:27:33 +0900

    Initial Commit

---
+++ ios-hands-on-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-hands-on-3
+//
+//  Created by junya.kawai on 2020/02/07.
+//  Copyright © 2020 nextbeat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
